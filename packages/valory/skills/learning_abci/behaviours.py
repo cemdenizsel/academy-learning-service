feda8d9ca394836e27b815db12037b5969e464c2
cem <cem.denizsel@tum.de>
2024-10-29 15:29:10 +0100

    add Coinmarketcap API.

--- packages/valory/skills/learning_abci/behaviours.py
+++ packages/valory/skills/learning_abci/behaviours.py
@@ -44,12 +44,14 @@ from packages.valory.skills.abstract_round_abci.behaviours import (
 from packages.valory.skills.abstract_round_abci.io_.store import SupportedFiletype
 from packages.valory.skills.learning_abci.models import (
     CoingeckoSpecs,
+    CoinmarketcapSpecs,
     Params,
     SharedState,
 )
 from packages.valory.skills.learning_abci.payloads import (
     DataPullPayload,
     DecisionMakingPayload,
+    PullCoinMarketCapPayload,
     TxPreparationPayload,
 )
 from packages.valory.skills.learning_abci.rounds import (
@@ -57,6 +59,7 @@ from packages.valory.skills.learning_abci.rounds import (
     DecisionMakingRound,
     Event,
     LearningAbciApp,
+    PullCoinMarketCapRound,
     SynchronizedData,
     TxPreparationRound,
 )
@@ -99,6 +102,12 @@ class LearningBaseBehaviour(BaseBehaviour, ABC):  # pylint: disable=too-many-anc
     def coingecko_specs(self) -> CoingeckoSpecs:
         """Get the Coingecko api specs."""
         return self.context.coingecko_specs
+
+    @property
+    def coinmarketcap_specs(self) -> CoinmarketcapSpecs:
+        """Get the Coingecko api specs."""
+        
+        return self.context.coinmarketcap_specs
 
     @property
     def metadata_filepath(self) -> str:
@@ -167,7 +176,7 @@ class DataPullBehaviour(LearningBaseBehaviour):  # pylint: disable=too-many-ance
         headers = {"accept": "application/json"}
 
         # Make the HTTP request to Coingecko API
-        response = yield from self.get_http_response(
+        response = yield from self.get_http_response( 
             method="GET", url=url, headers=headers
         )
 
@@ -201,6 +210,174 @@ class DataPullBehaviour(LearningBaseBehaviour):  # pylint: disable=too-many-ance
         price = response.get("usd", None)
         self.context.logger.info(f"Got token price from Coingecko: {price}")
         return price
+
+    def send_price_to_ipfs(self, price) -> Generator[None, None, Optional[str]]:
+        """Store the token price in IPFS"""
+        data = {"price": price}
+        price_ipfs_hash = yield from self.send_to_ipfs(
+            filename=self.metadata_filepath, obj=data, filetype=SupportedFiletype.JSON
+        )
+        self.context.logger.info(
+            f"Price data stored in IPFS: https://gateway.autonolas.tech/ipfs/{price_ipfs_hash}"
+        )
+        return price_ipfs_hash
+
+    def get_erc20_balance(self) -> Generator[None, None, Optional[float]]:
+        """Get ERC20 balance"""
+        self.context.logger.info(
+            f"Getting Olas balance for Safe {self.synchronized_data.safe_contract_address}"
+        )
+
+        # Use the contract api to interact with the ERC20 contract
+        response_msg = yield from self.get_contract_api_response(
+            performative=ContractApiMessage.Performative.GET_RAW_TRANSACTION,  # type: ignore
+            contract_address=self.params.olas_token_address,
+            contract_id=str(ERC20.contract_id),
+            contract_callable="check_balance",
+            account=self.synchronized_data.safe_contract_address,
+            chain_id=GNOSIS_CHAIN_ID,
+        )
+
+        # Check that the response is what we expect
+        if response_msg.performative != ContractApiMessage.Performative.RAW_TRANSACTION:
+            self.context.logger.error(
+                f"Error while retrieving the balance: {response_msg}"
+            )
+            return None
+
+        balance = response_msg.raw_transaction.body.get("token", None)
+
+        # Ensure that the balance is not None
+        if balance is None:
+            self.context.logger.error(
+                f"Error while retrieving the balance:  {response_msg}"
+            )
+            return None
+
+        balance = balance / 10**18  # from wei
+
+        self.context.logger.info(
+            f"Account {self.synchronized_data.safe_contract_address} has {balance} Olas"
+        )
+        return balance
+
+    def get_native_balance(self) -> Generator[None, None, Optional[float]]:
+        """Get the native balance"""
+        self.context.logger.info(
+            f"Getting native balance for Safe {self.synchronized_data.safe_contract_address}"
+        )
+
+        ledger_api_response = yield from self.get_ledger_api_response(
+            performative=LedgerApiMessage.Performative.GET_STATE,
+            ledger_callable="get_balance",
+            account=self.synchronized_data.safe_contract_address,
+            chain_id=GNOSIS_CHAIN_ID,
+        )
+
+        if ledger_api_response.performative != LedgerApiMessage.Performative.STATE:
+            self.context.logger.error(
+                f"Error while retrieving the native balance: {ledger_api_response}"
+            )
+            return None
+
+        balance = cast(int, ledger_api_response.state.body["get_balance_result"])
+        balance = balance / 10**18  # from wei
+
+        self.context.logger.error(f"Got native balance: {balance}")
+
+        return balance
+    
+class PullCoinMarketCapBehaviour(LearningBaseBehaviour):  # pylint: disable=too-many-ancestors
+    """This behaviours pulls token prices from API endpoints and reads the native balance of an account"""
+
+    matching_round: Type[AbstractRound] = PullCoinMarketCapRound
+
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            sender = self.context.agent_address
+
+            # First mehtod to call an API: simple call to get_http_response
+            #price = yield from self.get_token_price_simple()
+
+            # Second method to call an API: use ApiSpecs
+            # This call replaces the previous price, it is just an example
+            value = yield from self.get_latest_fear_greed_specs()
+
+            print("THE GREED AND FEAR VALUE IS: ", value)
+
+
+            # Store the price in IPFS
+            #value_ipfs_hash = yield from self.send_price_to_ipfs(value)
+
+            # Get the native balance
+            #native_balance = yield from self.get_native_balance()
+
+            # Get the token balance
+            #erc20_balance = yield from self.get_erc20_balance()
+
+            # Prepare the payload to be shared with other agents
+            # After consensus, all the agents will have the same price, price_ipfs_hash and balance variables in their synchronized data
+            payload = PullCoinMarketCapPayload(
+                sender=sender,
+                value=value,
+                #value_ipfs_hash=value_ipfs_hash,
+                #native_balance=native_balance,
+                #erc20_balance=erc20_balance,
+            )
+
+        # Send the payload to all agents and mark the behaviour as done
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+    def get_token_price_simple(self) -> Generator[None, None, Optional[float]]:
+        """Get token price from Coingecko usinga simple HTTP request"""
+
+        # Prepare the url and the headers
+        url_template = self.params.coingecko_price_template
+        url = url_template.replace("{api_key}", self.params.coingecko_api_key)
+        headers = {"accept": "application/json"}
+
+        # Make the HTTP request to Coingecko API
+        response = yield from self.get_http_response(
+            method="GET", url=url, headers=headers
+        )
+
+        # Handle HTTP errors
+        if response.status_code != HTTP_OK:
+            self.context.logger.error(
+                f"Error while pulling the price from CoinGecko: {response.body}"
+            )
+
+        # Load the response
+        api_data = json.loads(response.body)
+        price = api_data["autonolas"]["usd"]
+
+        self.context.logger.info(f"Got token price from Coingecko: {price}")
+
+        return price
+
+    def get_latest_fear_greed_specs(self) -> Generator[None, None, Optional[float]]:
+        """Get token price from Coinmarketcap using ApiSpecs"""
+        
+        # Get the specs
+        specs = self.coinmarketcap_specs.get_spec()
+    
+        # Make the call
+        raw_response = yield from self.get_http_response(**specs)
+
+        # Process the response
+        response = self.coinmarketcap_specs.process_response(raw_response)
+        self.context.logger.info(f"Got Fear and Greed PROCESSED response!!!!!!!!: {response}")
+
+        # Get the value
+        value = response.get("value", None)
+        self.context.logger.info(f"Got Fear and Greed index from CoinMarketCap: {value}")
+        return value
 
     def send_price_to_ipfs(self, price) -> Generator[None, None, Optional[str]]:
         """Store the token price in IPFS"""
@@ -658,6 +835,7 @@ class LearningRoundBehaviour(AbstractRoundBehaviour):
     abci_app_cls = LearningAbciApp  # type: ignore
     behaviours: Set[Type[BaseBehaviour]] = [  # type: ignore
         DataPullBehaviour,
+        PullCoinMarketCapBehaviour,
         DecisionMakingBehaviour,
         TxPreparationBehaviour,
     ]
